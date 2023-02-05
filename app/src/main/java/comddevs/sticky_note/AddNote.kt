786a7678a2320a92de61044a43de88afe755c9b2
pulokbd <abujafo34@gmail.com>
2023-02-05 21:57:23 +0600

    feat: edit note layout done

---
+++ app/src/main/java/comddevs/sticky_note/AddNote.kt
@@ -0,0 +1,11 @@
+package comddevs.sticky_note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNote : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
