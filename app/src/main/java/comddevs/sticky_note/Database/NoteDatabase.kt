8ef1e1dcc42e31f76e72598ca796eaa455b60103
pulokbd <abujafo34@gmail.com>
2023-02-05 22:25:47 +0600

    feat: room database necessary files setup done

---
+++ app/src/main/java/comddevs/sticky_note/Database/NoteDatabase.kt
@@ -0,0 +1,34 @@
+package comddevs.sticky_note.Database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import comddevs.sticky_note.Models.Note
+import comddevs.sticky_note.utilities.DATABASE_NAME
+
+@Database(entities = arrayOf(Note::class), version = 1, exportSchema = false)
+abstract class NoteDatabase :RoomDatabase(){
+
+    abstract fun getNoteDao() : NoteDao
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE : NoteDatabase? = null
+
+        fun getDatabase(context:Context):NoteDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDatabase::class.java,
+                    DATABASE_NAME
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/comddevs/sticky_note/utilities/constants.kt
@@ -0,0 +1,3 @@
+package comddevs.sticky_note.utilities
+
+const val DATABASE_NAME = "note_database"
